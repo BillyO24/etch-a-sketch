1d976999a416d61a46bcc89c271000acc943479a
Ian Wulf <ianwulf24@gmail.com>
2024-08-06 12:06:45 -0500

    Added button to prompt user to change grid size, which it does when clicked. Added button to reset grid to default on click.

--- game.js
+++ game.js
@@ -1,26 +1,57 @@
 //Create 16x16 default grid
 let body = document.querySelector("body");
 
-let grid = document.createElement("div");
-grid.classList.add("grid");
-body.appendChild(grid);
 
-for(let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for(let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.setAttribute("id", "square");
-        row.appendChild(square);
+function makeGrid(gridSize) {
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.setAttribute("id", "grid");
+    body.appendChild(grid);
+    for(let i = 0; i < gridSize; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < gridSize; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+    
+    let squares = document.querySelectorAll(".square");
+    
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", (event) => {
+            event.target.style["background-color"] = "red";
+        });
+    });
 }
 
-let squares = document.querySelectorAll(".square");
+//start off with default 16x16 grid
+makeGrid(16);
 
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", (event) => {
-        event.target.style["background-color"] = "red";
+let changeBtn = document.querySelector("button");
+
+changeBtn.addEventListener("click", function() {
+    let gridSize = prompt("Enter size you wish grid to be (E.g. enter 100 for up to 100x100):");
+    body.removeChild(grid);
+
+    makeGrid(gridSize);
+});
+
+let clearBtn = document.querySelector("#clear");
+
+clearBtn.addEventListener("click", function() {
+    let squares = document.querySelectorAll(".square");
+    
+    squares.forEach((square) => {
+        square.style["background-color"] = "white";
     });
+});
+
+let resetBtn = document.querySelector("#reset");
+
+resetBtn.addEventListener("click", function() {
+    body.removeChild(grid);
+    makeGrid(16);
 });
